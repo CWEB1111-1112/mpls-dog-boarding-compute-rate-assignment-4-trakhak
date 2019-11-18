e8654b920ef6ce23940594c33c46a2672647cd7b
Tran <trakhak@dunwoody.edu>
2019-11-17 21:14:57 -0600

    added code to housekeeping

--- Program.cs
+++ Program.cs
@@ -11,12 +11,42 @@ namespace assignment_four
             int dayCount;
             string code;
             double totalCost;
-            
+
             houseKeeping();
             ComputeRate(int dayCount);
             ComputeRate(int dayCount, string code);
             conclude(int dayCount,string code,double totalCost);
         }
+
+        static void houseKeeping(){
+            int dayCount;
+            Console.WriteLine("Enter how many days the dog will stay");
+            dayCount = Convert.ToInt32(Console.ReadLine());
+            return dayCount;
+
+            string code;
+            Console.WriteLine("Enter add-on code");
+            code = Console.ReadLine();
+            return code;
+
+        }
+
+        static int ComputeRate(){
+
+        }
+
+        static int ComputeRate(){
+
+        }
+
+        static void conclude(int dayCount,string code, double totalCost){
+
+        }
+        
+        do
+        {
+            
+        } while (true);
 
         //Overloaded method with one argument
 
